4e5f38fbd35e6077b89b9fc371fd84e850c931d9
GitHub Action <action@github.com>
2024-07-01 17:08:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/iantapply22/arcadeintellij/MyBundle.kt
+++ src/main/kotlin/com/github/iantapply22/arcadeintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.iantapply22.arcadeintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/iantapply22/arcadeintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/iantapply22/arcadeintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.iantapply22.arcadeintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
